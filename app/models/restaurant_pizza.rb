fcc136358179a16ab559a0b9bad196821b98af9a
Fleur41 <Muriithikings456@gmail.com>
2023-03-22 15:41:25 +0300

    updted models

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -1,4 +1,20 @@
 class RestaurantPizza < ApplicationRecord
-    belongs_to :restaurant
+    validates :pizza_id, presence: true
+    validates :restaurant_id, presence: true
+    validate :price_range
+
     belongs_to :pizza
+    belongs_to :restaurant
+
+
+    def price_range
+        if !price.nil?
+            if(price>30 && price < 1)
+                errors.add(:price, "must be between 30 and 1")
+            end
+        else
+            errors.add(:price, "must be between 30 and 1")
+        end
+    end
+
 end
